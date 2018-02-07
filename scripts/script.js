c303c70318d7f2835cc5d04b7c2896e3a107d8ef
DHRUV KUMAR <dkiphones620@gmail.com>
2018-02-07 19:00:36 +0530

    react controlled components forms

--- scripts/script.js
+++ scripts/script.js
@@ -1,47 +1,47 @@
 function PostButton(props) {
-  var style = {
-    width: 24,
-    height: 24
-  };
+    var style = {
+        width: 24,
+        height: 24
+    };
 
-  return (
-    <button style={style} onClick={() => props.handleClick()}>
+    return (
+        <button style={style} onClick={() => props.handleClick()}>
       {props.label}
     </button>
-  );
+    );
 }
 
 function PostText(props) {
-  var style = {
-    border: "1px solid black",
-    width: props.width
-  };
+    var style = {
+        border: "1px solid black",
+        width: props.width
+    };
 
-  return <div style={style}>{props.text}</div>;
+    return <div style={style}>{props.text}</div>;
 }
 
 function Post(props) {
-  var style = {
-    display: "flex"
-  };
-  return (
-    <div style={style}>
+    var style = {
+        display: "flex"
+    };
+    return (
+        <div style={style}>
       <PostButton label="x" handleClick={() => props.removeIt()} />
       <PostText text={props.title} width="200" />
       <PostButton label="-" handleClick={() => props.decrementScore()} />
       <PostText text={props.score} width="20" />
       <PostButton label="+" handleClick={() => props.incrementScore()} />
     </div>
-  );
+    );
 }
 
 function PostList(props) {
-  var list = props.postList.map((item, index) => {
-    if (item.title.length > 0) {
-      console.log(typeof item.title);
+    var list = props.postList.map((item, index) => {
+        if (item.title.length > 0) {
+            console.log(typeof item.title);
 
-      return (
-        <Post
+            return (
+                <Post
           title={
             item.title[0].toUpperCase() + item.title.slice(1, item.title.length)
           }
@@ -51,68 +51,78 @@ function PostList(props) {
           decrementScore={() => props.updateScore(index, -1)}
           removeIt={() => props.remove()}
         />
-      );
-    }
-  });
-  return <ol>{list}</ol>;
+            );
+        }
+    });
+    return <ol>{list}</ol>;
 }
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: "Enter post title",
-      items: []
-    };
-  }
-
-  onChange(event) {
-    this.setState({
-      value: event.target.value
-    });
-  }
-
-  addItem() {
-    var itemsCopy = this.state.items.slice();
-    var truncatedString = this.state.value.substring(0, 20);
-    itemsCopy.push({ title: truncatedString, score: 0 });
-    itemsCopy.sort((a, b) => {
-      return b.score - a.score;
-    });
-    this.setState({ items: itemsCopy, value: "" });
-  }
-
-  updateScore(index, val) {
-    var itemsCopy = this.state.items.slice();
-    itemsCopy[index].score += val;
-    itemsCopy.sort((a, b) => {
-      return b.score - a.score;
-    });
-    this.setState({ items: itemsCopy });
-  }
-
-  removeItem(index) {
-    var itemsCopy = this.state.items.slice();
-    itemsCopy.splice(index, 1);
-    itemsCopy.sort((a, b) => {
-      return b.score - a.score;
-    });
-
-    this.setState({ items: itemsCopy });
-  }
-
-  keyHandler(event) {
-    if (event.key == "Enter") {
-      this.addItem();
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: "Enter post title",
+            items: []
+        };
     }
-  }
 
-  render() {
-    return (
-      <div>
+    onChange(event) {
+        this.setState({
+            value: event.target.value
+        });
+    }
+
+    addItem() {
+        var itemsCopy = this.state.items.slice();
+        var truncatedString = this.state.value.substring(0, 20);
+        itemsCopy.push({
+            title: truncatedString,
+            score: 0
+        });
+        itemsCopy.sort((a, b) => {
+            return b.score - a.score;
+        });
+        this.setState({
+            items: itemsCopy,
+            value: ""
+        });
+    }
+
+    updateScore(index, val) {
+        var itemsCopy = this.state.items.slice();
+        itemsCopy[index].score += val;
+        itemsCopy.sort((a, b) => {
+            return b.score - a.score;
+        });
+        this.setState({
+            items: itemsCopy
+        });
+    }
+
+    removeItem(index) {
+        var itemsCopy = this.state.items.slice();
+        itemsCopy.splice(index, 1);
+        itemsCopy.sort((a, b) => {
+            return b.score - a.score;
+        });
+
+        this.setState({
+            items: itemsCopy
+        });
+    }
+
+    keyHandler(event) {
+        if (event.key == "Enter") {
+            this.addItem();
+        }
+    }
+
+    render() {
+        return (
+            <div>
         <input
           value={this.state.value}
-          onChange={this.onChange.bind(this)}
+          onChange={this.onChange.bind(this)  }
           onKeyPress={this.keyHandler.bind(this)}
         />
         <button onClick={() => this.addItem()}>Submit</button>
@@ -122,8 +132,8 @@ class App extends React.Component {
           remove={this.removeItem.bind(this)}
         />
       </div>
-    );
-  }
+        );
+    }
 }
 
 ReactDOM.render(<App />, document.getElementById("root"));
